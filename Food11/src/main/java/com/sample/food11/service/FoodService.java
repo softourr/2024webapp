460c85eeab55377dbc0369b42869a059e778826f
softourr <vpdrnls566@gmail.com>
2024-10-11 11:23:57 +0900

    Update FoodService.java

--- Food11/src/main/java/com/sample/food11/service/FoodService.java
+++ Food11/src/main/java/com/sample/food11/service/FoodService.java
@@ -2,8 +2,10 @@ package com.sample.food11.service;
 
 import com.sample.food11.api.request.CreateAndEditFoodRequest;
 import com.sample.food11.model.FoodEntity;
+import com.sample.food11.model.MenuEntity;
 import com.sample.food11.repository.FoodRepository;
 import com.sample.food11.repository.MenuRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,7 @@ public class FoodService {
     @Autowired
     private MenuRepository menuRepository;
 
+    @Transactional
     public FoodEntity createFood(
             CreateAndEditFoodRequest request
     ){
@@ -28,6 +31,18 @@ public class FoodService {
                         .build();
 
         foodRepository.save(food);
+
+        request.getMenus().forEach((menu)->{
+            MenuEntity menuEntity =
+                    MenuEntity.builder()
+                            .foodId(food.getId())
+                            .name(menu.getName())
+                            .price(menu.getPrice())
+                            .createdAt(ZonedDateTime.now())
+                            .updatedAt(ZonedDateTime.now())
+                            .build();
+            menuRepository.save(menuEntity);
+        });
 
         return food;
     }
